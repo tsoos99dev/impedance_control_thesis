72a9579103da75ec56110df10893952cb66c7a02
Tamás Soós <tsoos99@gmail.com>
2023-11-10 14:44:44 +0100

    Update controller chapter

--- models/indirect_compensated_minimum_order_controller.m
+++ models/indirect_compensated_minimum_order_controller.m
@@ -6,10 +6,13 @@ Be = 4;
 Ke = 16;
 
 impedance_model = tf(Ke, [Me Be Ke]);
-Pe = pole(impedance_model)'
+Pe = pole(impedance_model)';
 
 % Motor parameters
+% This has the biggest effect on how distorted 
+% the torque response is.
 J = 0.01;
+
 Km = 0.01;
 Bm = 0.1;
 L = 0.5;
@@ -20,9 +23,15 @@ Aaa = 0;
 Aab = [1 0]; 
 Aba = [0; 0];
 Abb = [-Bm/J Km/J; -Km/L -R/L];
+A_b = [Aab; Abb];
 A = [Aaa Aab; Aba Abb];
-Bt = [0; 1/J; 0];
-Bv = [0; 0; 1/L];
+
+Bat = 0;
+Bbt = [1/J; 0];
+Bt = [Bat; Bbt];
+Bav = 0;
+Bbv = [0; 1/L];
+Bv = [Bav; Bbv];
 B = [Bt Bv];
 C = [1 0 0];
 D = [0 0];
@@ -36,14 +45,19 @@ motor = ss(A, B, C, D, 'InputName', {'t', 'V'}, 'OutputName', 'a', 'StateName', 
 % ss2tf(A, B, C, D, 1)
 % ss2tf(A, B, C, D, 2)
 
-K = acker(A, Bv, P);
+K = place(A, Bv, P);
 Ka = K(1);
 Kb = K(2:end);
-Kee = acker(Abb', Aab', Po)';
+
+Ae = Abb - Bbt*[0 1 0]*A_b;
+Ko = place(Ae', Aab', Po)';
+
+% Guarantees the correct torque response steady state value.
+kc = (R+K(3))/Km-Ka/Ke;
 
 % Observer-controller dynamics
-AA = [A-Bv*K Bv*Kb; zeros(2, 3) Abb-Kee*Aab];
-BB = [Bv*K(1); zeros(2, 1)];
+AA = [A-Bv*K Bv*(Kb-[0 1 0]*A_b*kc); zeros(2, 3) Ae-Ko*Aab];
+BB = [Bv*Ka; zeros(2, 1)];
 CC = eye(5);
 DD = zeros(5, 1);
 c1 = ss(AA, BB, CC, DD, 'InputName', {'a0'}, 'OutputName', {'a', 'w', 'i', 'ew', 'ei'}, 'StateName', {'a', 'w', 'i', 'ew', 'ei'});
@@ -53,25 +67,23 @@ pc1 = pole(c1);
 % initial(c1(1, 1), [1; 0; 0; 0.5; 0; 0]);
 % [num, den] = ss2tf(AA, BB, CC, DD, 1)
 
-% [N, D] = ss2tf(AA, BB, CC, DD)
+% ss2tf(AA, BB, CC, DD)
 
 % Disturbance response
-AA = [A-Bv*K Bv*Kb; zeros(2, 3) Abb-Kee*Aab];
-BB = [Bt; Bt(2:end)];
+AA = [A-Bv*K Bv*(Kb-[0 1 0]*A_b*kc); zeros(2, 3) Ae-Ko*Aab];
+BB = [Bt - Bv*kc; zeros(2, 1)];
 CC = eye(5);
 DD = zeros(5, 1);
 c2 = ss(AA, BB, CC, DD, 'InputName', {'a0'}, 'OutputName', {'a', 'w', 'i', 'ew', 'ei'}, 'StateName', {'a', 'w', 'i', 'ew', 'ei'});
 O = s*eye(5) - AA;
 vpa(subs(collect(CC/O*BB), s, 0))
-% tzero(c2(1))
-
-palette = ["#79addc" "#ffc09f" "#ffee93" "#fcf5c7" "#adf7b6"];
 
 figure;
 pbaspect([8,6,1])
 set(gcf,'color','w');
 set(gca, 'FontName', 'Helvetica');
 hold on;
+ylim([0, 1.2])
 
 tmax = 5;
 yline(1, '--', Color='black', LineWidth=2, Alpha=0.2)
@@ -79,10 +91,6 @@ yline(1, '--', Color='black', LineWidth=2, Alpha=0.2)
 s1 = plot(t, y, 'Color', palette(1), 'LineWidth', 2);
 [y, t] = step(c1(1), tmax);
 s2 = plot(t, y, 'Color', palette(2), 'LineWidth', 2);
-% [y, t] = step(c1(5), tmax);
-% s1 = plot(t, y, 'Color', palette(2), 'LineWidth', 2);
-% [y, t] = step(c2(1), tmax);
-% s3 = plot(t, y, 'Color', palette(2), 'LineWidth', 2);
 
 xlabel('Id\H o $[s]$', 'Interpreter', 'latex', 'FontSize', 16)
 ylabel('Sz\"ogelfordul\''as $\left[rad\right]$', 'Interpreter', 'latex', 'FontSize', 16)
@@ -96,8 +104,8 @@ set(gca, 'Box', 'off', 'TickDir', 'out', 'TickLength', [.02 .02], ...
     'XColor', [.3 .3 .3], 'YColor', [.3 .3 .3], ...
     'LineWidth', 1)
 
-set(gcf,'PaperPositionMode','auto')
-export_fig("images/observer_controller_pos_resp.png", "-png", "-m4", "-r300")
+% set(gcf,'PaperPositionMode','auto')
+% export_fig("observer_controller_pos_resp.png", "-png", "-m4", "-r300")
 
 figure;
 pbaspect([8,6,1])
@@ -124,20 +132,3 @@ set(gca, 'Box', 'off', 'TickDir', 'out', 'TickLength', [.02 .02], ...
     'XMinorTick', 'on', 'YMinorTick', 'on', 'YGrid', 'on', ...
     'XColor', [.3 .3 .3], 'YColor', [.3 .3 .3], ...
     'LineWidth', 1)
-
-set(gcf,'PaperPositionMode','auto')
-export_fig("images/observer_controller_torque_resp.png", "-png", "-m4", "-r300")
-% 
-% % poles(K/(s*eye(3) - A + Ke*C + Bv*K)*Ke)
-% 
-% % figure;
-% % xl = [-30, 1];
-% % subplot(3, 2, 2)
-% % pzmap(impedance_model)
-% % xlim(xl);
-% % subplot(3, 2, 4)
-% % pzmap(c1(1))
-% % xlim(xl);
-% % subplot(3, 2, 6)
-% % pzmap(c2(1))
-% % xlim(xl);
---
+++ models/pole_placement_conditions.m
@@ -0,0 +1,45 @@
+syms s 
+syms Me Be Ke Bm Km J L R real
+syms po real
+
+impedance_model = Ke/(s^2*Me + s*Be + Ke);
+
+Aaa = 0;
+Aab = [1 0]; 
+Aba = [0; 0];
+Abb = [-Bm/J Km/J; -Km/L -R/L];
+A = [Aaa Aab; Aba Abb];
+Bt = [0; 1/J; 0];
+Bv = [0; 0; 1/L];
+B = [Bt Bv];
+C = [1 0 0];
+D = [0 0];
+
+pd = (Me*A^3 + (Be-Me*po)*A^2 + (Ke-Be*po)*A - Ke*po*eye(3))/Me;
+Cm = [Bv A*Bv A^2*Bv];
+
+K = [0 0 1]/Cm*pd;
+Ka = K(1);
+Kb = K(2:end);
+
+pd = (Abb')^2-2*Abb'*po + eye(2)*po^2;
+Cm = [Aab' Abb'*Aab'];
+Kee = ([0 1]/Cm*pd)';
+
+% vpa(subs(Kee, [L R J Km Bm Me Be Ke po], [0.2 1 0.01 0.01 0.1 1 4 16 -2]))
+
+AA = [A-Bv*K Bv*Kb; zeros(2, 3) Abb-Kee*Aab];
+BB = [Bv*K(1); zeros(2, 1)];
+CC = eye(5);
+DD = zeros(5, 1);
+
+% pc = det(s*eye(3)-A+Bv*K)
+% det(s*eye(2)-Abb+Kee*Aab)
+
+s1 = simplify(CC/(s*eye(5)-AA)*BB + DD);
+ds1 = s1(1);
+pretty(partfrac(ds1))
+pretty(simplify(ilaplace(ds1/s)))
+fplot(subs(ilaplace(ds1/s), [L R J Km Bm Me Be Ke po], [0.2 1 0.01 0.01 0.1 1 4 16 -2]), [0 5])
+hold on;
+fplot(subs(ilaplace(impedance_model/s), [L R J Km Bm Me Be Ke po], [0.2 1 0.01 0.01 0.1 1 4 16 -2]), [0 5])
